2b607c60b9555559d18a302ff1afff660d0b0093
ilisien <ian8lisien@gmail.com>
2025-02-08 18:02:15 -0500

    debug, get_koppen_climate functions working sort of

--- climate.py
+++ climate.py
@@ -1,5 +1,10 @@
-import rasterio
+import rasterio, os
 import numpy as np
+
+import matplotlib.pyplot as plt
+from rasterio.plot import show
+
+from download_climate_data import download_climate_data
 
 # see https://www.nature.com/articles/sdata2018214
 KOPPEN_CLASSES = {
@@ -27,3 +32,33 @@ KOPPEN_CLASSES = {
     22: "ET - Tundra",
     23: "EF - Ice Cap"
 }
+
+climate_data_path = "climate_data/koppen_geiger_0p1.tif"
+
+def get_koppen_climate(lat,lon):
+    if not os.path.exists(climate_data_path):
+        download_climate_data()
+
+    with rasterio.open(climate_data_path) as dataset:
+        row,col = dataset.index(lon,lat)
+        climate_value = dataset.read(1)[row,col]
+        print(climate_value)
+
+        return KOPPEN_CLASSES.get(climate_value, "Unknown Climate Zone")
+
+def debug_data():
+    with rasterio.open(climate_data_path) as dataset:
+        data = dataset.read(1)
+        unique_values = np.unique(data)
+        print(unique_values)
+        fig, ax = plt.subplots(figsize=(10, 6))
+        show(dataset, ax=ax, cmap="jet")
+        ax.set_title("Köppen-Geiger Climate Classification")
+        plt.show()
+    
+
+if __name__ == "__main__":
+    #lat, lon = 45.0, -93.0
+    #koppen_climate = get_koppen_climate(lat, lon)
+    #print(koppen_climate)
+    debug_data()
