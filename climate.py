cc6a814e2a41753f62c9d003815df8e4b890c6c6
ilisien <ian8lisien@gmail.com>
2025-02-08 18:19:27 -0500

    get_koppen_climate function working well

--- climate.py
+++ climate.py
@@ -1,53 +1,33 @@
 import rasterio, os
 import numpy as np
-
 import matplotlib.pyplot as plt
 from rasterio.plot import show
+import pandas as pd
 
 from download_climate_data import download_climate_data
 
-# see https://www.nature.com/articles/sdata2018214
-KOPPEN_CLASSES = {
-    1: "Af - Tropical Rainforest",
-    2: "Am - Tropical Monsoon",
-    3: "Aw - Tropical Savanna",
-    4: "BWh - Hot Desert",
-    5: "BWk - Cold Desert",
-    6: "BSh - Hot Steppe",
-    7: "BSk - Cold Steppe",
-    8: "Csa - Hot-summer Mediterranean",
-    9: "Csb - Warm-summer Mediterranean",
-    10: "Csc - Cold-summer Mediterranean",
-    11: "Cfa - Humid Subtropical",
-    12: "Cfb - Marine West Coast",
-    13: "Cfc - Subpolar Oceanic",
-    14: "Dsa - Hot-summer Continental",
-    15: "Dsb - Warm-summer Continental",
-    16: "Dsc - Cold-summer Continental",
-    17: "Dsd - Continental Subarctic",
-    18: "Dfa - Humid Continental",
-    19: "Dfb - Warm-summer Humid Continental",
-    20: "Dfc - Subarctic",
-    21: "Dfd - Extreme Subarctic",
-    22: "ET - Tundra",
-    23: "EF - Ice Cap"
-}
+climate_data_tif_path = "climate_data/koppen_geiger_0p1.tif"
+koppen_table = "climate_data/koppen_table.csv"
 
-climate_data_path = "climate_data/koppen_geiger_0p1.tif"
+KOPPEN_CLASSES = pd.read_csv(koppen_table).set_index("Class")
+
+def get_koppen_major(number):
+    try:
+        return KOPPEN_CLASSES.loc[number,"Major"]
+    except:
+        return -1
 
 def get_koppen_climate(lat,lon):
-    if not os.path.exists(climate_data_path):
+    if not os.path.exists(climate_data_tif_path):
         download_climate_data()
 
-    with rasterio.open(climate_data_path) as dataset:
+    with rasterio.open(climate_data_tif_path) as dataset:
         row,col = dataset.index(lon,lat)
         climate_value = dataset.read(1)[row,col]
-        print(climate_value)
-
-        return KOPPEN_CLASSES.get(climate_value, "Unknown Climate Zone")
+        return get_koppen_major(climate_value), climate_value
 
 def debug_data():
-    with rasterio.open(climate_data_path) as dataset:
+    with rasterio.open(climate_data_tif_path) as dataset:
         data = dataset.read(1)
         unique_values = np.unique(data)
         print(unique_values)
@@ -58,7 +38,7 @@ def debug_data():
     
 
 if __name__ == "__main__":
-    #lat, lon = 45.0, -93.0
-    #koppen_climate = get_koppen_climate(lat, lon)
-    #print(koppen_climate)
-    debug_data()
+    lat, lon = 45.0, -93.0
+    koppen_climate = get_koppen_climate(lat, lon)
+    print(koppen_climate)
+    #debug_data()
