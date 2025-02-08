7d7249f29940dce934ac6d4452b6af7d9c498169
ilisien <ian8lisien@gmail.com>
2025-02-07 22:10:32 -0500

    debug in observation class

--- apiinteract.py
+++ apiinteract.py
@@ -21,29 +21,45 @@ class INatObservation:
         self.last_reviewed = None # would normally be a datetime object
 
         # object data
-        self.user = self.boxed_raw.user
-        self.grade = self.boxed_raw.quality_grade
-        self.geoprivacy = self.boxed_raw.taxon_geoprivacy
+        self.author = self.get_author_title()
+        #self.grade = self.boxed_raw.quality_grade (shouldn't ever be needed; need to be filtering for this in the api calls)
         self.uuid = self.boxed_raw.uuid
-        self.species_guess = self.boxed_raw.species_guess
+        self.sci_name = self.boxed_raw.taxon.name.lower()
+        self.common_name = self.boxed_raw.taxon.preferred_common_name.lower()
 
-        self.place_guess = self.boxed_raw.place_guess
+        self.place = self.boxed_raw.place_guess.lower()
+        self.location = self.boxed_raw.location
+
         self.identifications = self.boxed_raw.identifications
         self.project_observations = self.boxed_raw.project_observations
         self.photos = self.boxed_raw.photos
         self.user = self.boxed_raw.user
 
         self.geoprivacy = self.boxed_raw.geoprivacy
-        self.location = self.boxed_raw.location
+        
 
         self.taxon = self.boxed_raw.taxon
 
         self.num_identification_agreements = self.boxed_raw.num_identification_agreements
         self.num_identification_disagreements = self.boxed_raw.num_identification_disagreements
+    
+    def get_author_title(self):
+        user = self.boxed_raw.user
+        return f"{user.name} ({user.login})"
+    
+    def debug(self):
+        print(f"author: {self.author}")
+        print(f"uuid: {self.uuid}")
+        print(f"sci_name: {self.sci_name}")
+        print(f"common_name: {self.common_name}")
+        print(f"place: {self.place}")
+        print(f"location: {self.location}")
 
         
 
 # "grab_species_list" should obtain a list of api objects that conform to the 
 # parsed observation object standard set out by the above class 
 
-print(Box(inat.get_observations(user_id='ilisien',taxon_name="fulvifomes robiniae")).results[0].community_taxon_id)
+if __name__ == "__main__":
+    fungus = INatObservation(Box(inat.get_observations(user_id='ilisien',taxon_name="fulvifomes robiniae")).results[0])
+    fungus.debug()
